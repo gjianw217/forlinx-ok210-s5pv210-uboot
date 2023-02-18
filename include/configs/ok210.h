320d5f6b7db2f985ffb94808b7e79c98fcdf81f1
koudaiwulian <gjianw217@163.com>
2023-02-18 21:11:07 +0800

    add the uart2 to atc as console

--- include/configs/ok210.h
+++ include/configs/ok210.h
@@ -57,7 +57,7 @@
 /*
  * select serial console configuration
  */
-#define CONFIG_SERIAL0			1	/* use SERIAL 0 on OK210 */
+#define CONFIG_SERIAL2			1	/* use SERIAL 0 on OK210 */
 
 /* PWM */
 #define CONFIG_PWM			1
